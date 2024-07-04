02c00b3c1ab50b3b774c608e309813b0650240c2
Тим Гусь <guskov.tima@rambler.ru>
2024-07-04 16:11:51 +0300

    Добавьте файлы проекта.

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("add")]
+        public IActionResult Add(double a, double b) => Ok(a + b);
+
+        [HttpGet("subtract")]
+        public IActionResult Subtract(double a, double b) => Ok(a - b);
+
+        [HttpGet("multiply")]
+        public IActionResult Multiply(double a, double b) => Ok(a * b);
+
+        [HttpGet("divide")]
+        public IActionResult Divide(double a, double b)
+        {
+            if (b == 0)
+                return BadRequest("Division by zero is not allowed.");
+            return Ok(a / b);
+        }
+    }
+}
